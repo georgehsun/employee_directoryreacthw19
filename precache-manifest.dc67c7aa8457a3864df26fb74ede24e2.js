227754c8bae7a6007800228111584cec4c7b3fcd
George Sun <Gsunems@gmail.com>
2020-04-28 18:41:16 -0500

    Updates

---
+++ precache-manifest.dc67c7aa8457a3864df26fb74ede24e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4519d21b6f1c6f93a92282f7ac309c79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ed0578f589aabc83266",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e9248dfa5d949217607",
+    "url": "/static/js/2.8de52f21.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8de52f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed0578f589aabc83266",
+    "url": "/static/js/main.134e4718.chunk.js"
+  },
+  {
+    "revision": "2a0557d5098a63788aae",
+    "url": "/static/js/runtime-main.27153d7b.js"
+  }
+]);
